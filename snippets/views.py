59ed2584b45c06b1114c720f4b438705dd4bec85
ervaibhavkumar <vk530873@gmail.com>
2019-07-27 16:04:28 +0530

    Added authentication lesson

--- snippets/views.py
+++ snippets/views.py
@@ -88,11 +88,15 @@
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins, generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 # class SnippetList(APIView):
 #     def get(self, request, format=None):
@@ -131,8 +135,6 @@ from rest_framework import status
 #         snippet.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
-from rest_framework import mixins, generics
-
 # class SnippetList(mixins.ListModulemixin, mixins.CreateModuleMixin, generics.GenericAPIView):
 #     queryset = Snippet.objects.all()
 #     serializer_class = SnippetSerializer
@@ -158,8 +160,21 @@ from rest_framework import mixins, generics
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
-    serialzer_class = SnippetSerializer
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
